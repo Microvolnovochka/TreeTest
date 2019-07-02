dbd460bfc900e5a0e4bf4021e5485bdd6d295c72
Matvey Karabanov <matveikarabanov@mail.ru>
2019-07-02 17:15:42 +0300

    draw but shitty

--- main_js_tree.js
+++ main_js_tree.js
@@ -1,30 +1,104 @@
-var jstree = document.getElementById("tree").getElementsByTagName("ul")[0];
-var jstreeleaf = document.getElementById("tree").getElementsByTagName("li");
+var jstree = document.getElementById("tree");
+var obj =[ 
+    {
+    value: "root",
+    gen: 0,
+    children:2,
+    childrenpointers: [1,2]
+    },
+    {
+    value: "children",
+    gen: 1,
+    children: 1,
+    childrenpointers: [3]
+    },
+    {
+    value: "children",
+    gen: 1,
+    children: 0,
+    childrenpointers: []
+    },
+    {
+    value: "children",
+    gen: 2,
+    children: 0,
+    childrenpointers: []
+    }
+];
 
-for (let i=0;i<jstreeleaf.length;i++)
-{
-    let leaf = jstreeleaf[i];
-    let span = document.createElement("span");
+(function visualtree (obj) {
+    if (typeof obj !="object")
+    {return;}
+    if (!obj)
+    {return;}
+    var parent =jstree;
 
-    leaf.insertBefore(span,leaf.firstChild);
-    span.appendChild(span.nextSibling);
-}
+    drawnod(obj[0]);
 
-jstree.onclick = function (event)
+    function drawnod (element){
+        var newUl = document.createElement("ul");
+        var newLi = document.createElement("li");
+        var span = document.createElement("span");
+        var divname = document.createElement("div");
+        var divplus = document.createElement("div");
+        var divminus = document.createElement("div");
+        var temparent;
+
+        /*if (0==element.gen)
+        {
+            newLi.innerText = element.value;
+        }
+        else 
+        {
+            newLi.innerText = element.value +" " + element.gen;
+        }*/
+        divname.innerHTML = element.value + " " + element.gen + " ";
+        divplus.innerHTML = "+";
+        divplus.id = "plus";
+        divminus.innerHTML = " -";
+        divminus.id="minus";
+        newLi.appendChild(divname);
+        newLi.appendChild(divplus);
+        newLi.appendChild(divminus);
+        newLi.insertBefore(span,newLi.firstChild);
+        span.appendChild(span.nextSibling);
+        newUl.appendChild(newLi);
+        parent.appendChild(newUl);
+        if (element.children)
+        {
+            temparent = parent;
+            parent = newLi;
+            for (let i=0;i<element.children;i++)
+            {
+                drawnod(obj[element.childrenpointers[i]]);
+            }
+            parent = temparent;
+        }
+        else
+        {return;}
+    }
+})(obj);
+
+var plus = document.getElementById("plus");
+
+plus.onclick = function (event)
 {
     var target = event.target;
+    alert("plus");
 
-    if (target.tagName != "SPAN")
+    /*if (target.tagName != "SPAN")
     {
-        alert("click without span");
         return;
-    }
-    alert ("click with span");
+    }*/
     var treechild = document.createElement("ul");
     var leaf = document.createElement("li");
     var span = document.createElement("span");
+    let plus = document.createElement("div");
 
+    plus.id = "plus";
+    plus.innerHTML="+";
     leaf.innerText = "Child";
+    leaf.appendChild(plus);
     leaf.insertBefore(span,leaf.firstChild);
     span.appendChild(span.nextSibling);
 
