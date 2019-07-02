75676cb99c51fda2d33dc2d45f31eaa51acbe6b7
Matvey Karabanov <matveikarabanov@mail.ru>
2019-07-02 13:36:47 +0300

    First work proto

--- main_js_tree.js
+++ main_js_tree.js
@@ -1,11 +1,33 @@
-var jstree = document.getElementsByTagName("ul")[0];
-var jstreeleaf = document.getElementsByTagName("li");
+var jstree = document.getElementById("tree").getElementsByTagName("ul")[0];
+var jstreeleaf = document.getElementById("tree").getElementsByTagName("li");
 
 for (let i=0;i<jstreeleaf.length;i++)
 {
-    var leaf = jstreeleaf[i];
+    let leaf = jstreeleaf[i];
+    let span = document.createElement("span");
 
-    var span = document.createElement("span");
     leaf.insertBefore(span,leaf.firstChild);
     span.appendChild(span.nextSibling);
+}
+
+jstree.onclick = function (event)
+{
+    var target = event.target;
+
+    if (target.tagName != "SPAN")
+    {
+        alert("click without span");
+        return;
+    }
+    alert ("click with span");
+    var treechild = document.createElement("ul");
+    var leaf = document.createElement("li");
+    var span = document.createElement("span");
+
+    leaf.innerText = "Child";
+    leaf.insertBefore(span,leaf.firstChild);
+    span.appendChild(span.nextSibling);
+
+    treechild.appendChild(leaf);
+    target.appendChild(treechild);
 }
